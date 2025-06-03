90fcbc5d05817117f69d467ad77c32dc897e348a
Waspish <ostapenko.andreik@gmail.com>
2025-06-04 00:32:16 +0300

    Change all for Safari

--- test.py
+++ test.py
@@ -1,13 +1,7 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
 
-options = webdriver.ChromeOptions()
-options.add_argument("--headless=new")  # for CI environments
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
-
-driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
+# Safari doesn't require options or external driver managers
+driver = webdriver.Safari()
 
 print("CI/CD is working!")
 
